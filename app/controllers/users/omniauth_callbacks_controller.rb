86a412b703d58d91baac81b716d9e53d0fd9f38e
Tejas Dinkar <tejas@gja.in>
2018-09-02 09:14:46 +0530

    Handled Regular Login Too

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     sign_in user if user.persisted?
 
     respond_to do |format|
-      format.json { render json: user }
+      format.json { render json: user, status: user.persisted? ? 201 : 400 }
+      format.html do
+        if user.persisted?
+          redirect_to root_path, event: :authentication
+          set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+        else
+          session["devise.facebook_data"] = request.env["omniauth.auth"]
+          redirect_to new_user_registration_url
+        end
+      end
     end
   end
 
