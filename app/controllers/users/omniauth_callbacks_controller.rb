8ff3b05ebf16ad7abfbd8bf8352844adb42e60e4
prajiltp <tpprajilkottur@gmail.com>
2018-09-08 10:25:32 +0530

    google authentication

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,29 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   respond_to :html, :json
 
-  def facebook
-    user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in user if user.persisted?
 
-    respond_to do |format|
-      format.json { render json: user, status: user.persisted? ? 201 : 400 }
-      format.html do
-        if user.persisted?
-          redirect_to root_path, event: :authentication
-          set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-        else
-          session["devise.facebook_data"] = request.env["omniauth.auth"]
-          redirect_to new_user_registration_url
+  %w(facebook google_oauth2).each do |provider|
+    define_method "#{provider}" do
+      user = User.from_omniauth(request.env["omniauth.auth"])
+      user.save()
+      sign_in user if user.persisted?
+
+      respond_to do |format|
+        format.json { render json: user, status: user.persisted? ? 201 : 400 }
+        format.html do
+          if user.persisted?
+            redirect_to root_path, event: :authentication
+            set_flash_message(:notice, :success, kind: request.env['omniauth.auth']['provider']) if is_navigational_format?
+          else
+            session["devise.facebook_data"] = request.env["omniauth.auth"]
+            redirect_to new_user_registration_url
+          end
         end
       end
     end
-  end
 
-  def failure
-    redirect_to root_path
+    def failure
+      redirect_to root_path
+    end
   end
 end
