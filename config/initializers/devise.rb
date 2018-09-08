f7be534910873613ae674b253aa43b78858cf696
prajiltp <tpprajilkottur@gmail.com>
2018-09-08 10:25:32 +0530

    google authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,7 +257,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :facebook, Rails.application.credentials.facebook[:app_id], Rails.application.credentials.facebook[:secret], scope: 'public_profile,email'
-
+  config.omniauth :google_oauth2, Rails.application.credentials.google[:app_id], Rails.application.credentials.google[:secret]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
